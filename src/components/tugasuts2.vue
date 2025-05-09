26525baa39173c8513d07e65ec1e6dcc51369e6d
unknown <mamamdulu06@gmail.com>
2025-05-09 17:26:48 +0700

    add : 4 data from list

--- src/components/tugasuts2.vue
+++ src/components/tugasuts2.vue
@@ -12,7 +12,7 @@ const todos = ref([
   { id: id++, text: 'Pull-up Bar', done: false }
 ])
 //membuat delate and update
-//menambahkan 4 data 
+//menambahkan 4 data list 
 const filteredTodos = computed(() => {
   return hideCompleted.value
     ? todos.value.filter((t) => !t.done)
